02253f12f788932a2cb82b77a85f9733b0e9383a
akshay pawar <ap3690700@gmail.com>
2026-05-05 00:41:42 +0530

    Fix case sensitivity in About image import path

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import aboutImg from "../assets/about.webp";
+import aboutImg from "../assets/About.webp";
 import { About_text } from "../constants";
 import { motion } from "framer-motion";
 
