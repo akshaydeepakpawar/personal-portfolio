3a97d10483729fd6d6b6733f4b801e2a95c79745
akshay pawar <ap3690700@gmail.com>
2026-05-05 00:51:57 +0530

    Update Experience component layout for better responsiveness

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -20,7 +20,7 @@ const Experience = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+              className="text-center lg:w-1/2"
             >
               <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
             </motion.div>
